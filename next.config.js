074a02235153da31112fb4c73a1de30d60772640
ssongkr <ssongkr@gmail.com>
2021-05-23 20:03:05 +0900

    경로 엘리어스 추가

--- next.config.js
+++ next.config.js
@@ -3,11 +3,15 @@ const path = require('path');
 module.exports = {
   webpack (config) {
     Object.assign(config.resolve.alias, {
-      '~': path.resolve(__dirname),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@libs': path.resolve(__dirname, './src/libs'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@styles': path.resolve(__dirname, './src/styles'),
     });
     return config;
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, 'src/styles')],
   },
 };
